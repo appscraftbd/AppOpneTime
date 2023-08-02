dbfc12445ecfd4dc9a71a5b621a0b7d7b6167600
appscraft.bd <appscraftbd@gmail.com>
2023-08-02 10:20:52 +0600

    1.0.1

---
+++ app/src/main/java/com/example/appopentime/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.appopentime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    private TextView textView;
+
+/////////////////////////////////////////
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int inte = 0;
+
+
+        textView = findViewById(R.id.tvdisplay);
+        sharedPreferences = getSharedPreferences("data",MODE_PRIVATE);
+        String out = sharedPreferences.getString("name","Default value");
+        textView.setText(""+out);
+
+        editor = sharedPreferences.edit();
+
+
+        int inumber = Integer.parseInt(out);
+        inte = inumber+1;
+        String in= Integer.toString(inte);
+        editor.putString("name",""+in);
+        editor.apply();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
